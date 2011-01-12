c0d6bfd6b3bdd43f1d330fccedef34aab4216f80
John Perry <jgperry@gmail.com>
2011-01-12 12:57:02 -0500

    Added more relevent tests for Voter and Status model method tests

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -2,12 +2,45 @@ require 'spec_helper'
 
 describe Status do
   before(:each) do
-    bill = Factory.create( :bill )
-    Factory.create( :status, :bill => bill )
+    @bill = Factory.create( :bill )
+    Factory.create( :status, :bill => @bill )
   end
   
   it "should provide most recent" do
     status = Status.most_recent.first
     status.statusDate.to_s.should match( /2011-01-10/ )
+  end
+  
+  describe "last_date method" do
+    before( :each ) do
+      Factory.create( :status, :bill => @bill, :statusDate => '2011-01-05 12:00:00' )
+    end
+
+    it "should match find max date result" do
+      Status.maximum( "DATE(statusDate)" ).should == Status.last_date
+    end
+    
+    it "should not match older dates" do
+      Status.last_date.should_not be < '2011-01-10'
+    end
+  end
+  
+  describe "most_recent method" do
+    before( :each ) do
+      Factory.create( :status, :bill => @bill, :statusDate => '2011-01-05 12:00:00' )
+    end
+
+    it "should include all statuses on most recent date" do
+      Factory.create( :status, :bill=> @bill, :statusDate => '2011-01-10 15:00:00')
+      Status.where( "DATE(statusDate) = ?", Status.last_date ).should == Status.most_recent
+    end
+    
+    it "should not include statuses from older dates" do
+      Status.most_recent.each do |recent|
+        Status.where( "DATE(statusDate) != ?", Status.last_date ).each do |old|
+          recent.should_not == old
+        end
+      end
+    end
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -11,4 +11,37 @@ describe Vote do
     vote = Vote.most_recent.first
     vote.date.to_s.should match( /2011-01-10/ )
   end
+
+  describe "last_date method" do
+    before( :each ) do
+      Factory.create( :vote, :bill => @bill, :date => '2011-01-05 12:00:00' )
+    end
+
+    it "should match find max date result" do
+      Vote.maximum( "DATE(date)" ).should == Vote.last_date
+    end
+    
+    it "should not match older dates" do
+      Vote.last_date.should_not be < ( '2011-01-10' )
+    end
+  end
+  
+  describe "most_recent method" do
+    before( :each ) do
+      Factory.create( :vote, :bill => @bill, :date => '2011-01-05 12:00:00' )
+    end
+
+    it "should include all statuses on most recent date" do
+      Factory.create( :vote, :bill=> @bill, :date => '2011-01-10 15:00:00')
+      Vote.where( "DATE(date) = ?", Vote.last_date ).should == Vote.most_recent
+    end
+    
+    it "should not include statuses from older dates" do
+      Vote.most_recent.each do |recent|
+        Vote.where( "DATE(date) != ?", Vote.last_date ).each do |old|
+          recent.should_not == old
+        end
+      end
+    end
+  end
 end
