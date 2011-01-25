6e36eef7b37bbe284c87dabc4dd70bb0eb3f7fbc
John Perry <jgperry@ajc.com>
2011-01-25 16:00:43 -0500

    Implement bills#show page with bill number and title, status histoy and votes

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,9 +1,12 @@
 class BillsController < ApplicationController
   def index
+    @title = 'Search results'
     @bills = Bill.all
   end
 
   def show
+    @bill = Bill.find(params[:id])
+    @title = @bill.number
   end
 
   def search
