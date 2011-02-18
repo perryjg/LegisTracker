b68722c61935c13a52a9efe68d758431ab0aed6f
John Perry <jgperry@gmail.com>
2011-02-18 17:43:02 -0500

    cleaned up home controller and specs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,7 @@
 class HomeController < ApplicationController
   def index
     @title = 'Home'
-    stdate = Status.last_date
-    @status_date = params[:status_date] || Status.last_date.to_s
-    @hot_events = Status.hot.by_date_desc
-    @events = Status.find_for_date( @status_date )
-    
-    vdate = Vote.last_date
-    @vote_date = params[:vote_date] || Vote.last_date.to_s
+    @events = Status.hot.by_date_desc
     @votes = Vote.hot_bills.order_by_datetime_desc
     
     @house_rss = HouseFeed.find_recent
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,6 +4,9 @@ describe HomeController do
   describe HomeController, '#index' do
     before( :each ) do
       @bill = Factory.create( :bill )
+      @bill.hot_list.add( 'hot' )
+      @bill.save
+      
       @status = Factory.create( :status, :bill => @bill )
       @votes = Factory.create( :vote, :bill => @bill )
       @house_feed = Factory.create( :house_feed )
@@ -22,19 +25,9 @@ describe HomeController do
       { :get => root_path }.should route_to( :controller => "home", :action => "index" )
     end
 
-    describe "Show recent bill events" do
-      it { should assign_to( :status_date ).with( Status.last_date.to_s ) }
-      it { should assign_to( :events ).with( Status.find_for_date( Status.last_date.to_s ) ) }
-      it { should assign_to( :vote_date ).with( Vote.last_date.to_s ) }
-      it { should assign_to( :votes ).with( Vote.find_for_date( Vote.last_date.to_s ) ) }
-      
-      it "should assign @hot_events" do
-        @hot_event = Factory( :bill )
-        @hot_event.hot_list.add( 'hot' )
-        @hot_event.save
-        
-        assigns[:hot_events].should == Status.hot.most_recent
-      end
+    describe "Show watched bill events" do
+      it { should assign_to( :events ).with( Status.hot.by_date_desc ) }
+      it { should assign_to( :votes ).with( Vote.hot_bills.order_by_datetime_desc ) }
     end
     
     describe "Show recent press releases" do
