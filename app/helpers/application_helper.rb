3416d391995dd0776dc02821d0adac430f7d8aa0
Chuck Blevins <crblev@gmail.com>
2011-03-01 13:49:12 -0500

    tab navigation added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def current_class?(test_path)
+    return 'current' if request.request_uri == test_path
+    ''
+  end
+  
+  def current1_class?(test_path)
+    return 'current1' if request.request_uri == test_path
+    ''
+  end
+  
+  
 end
